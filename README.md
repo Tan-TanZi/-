61b98923ae2843c45f4e9821b0b8e314e20a0105
Tan-TanZi <142204787+Tan-TanZi@users.noreply.github.com>
2023-11-06 22:25:01 +0800

    Add files via upload
    2023-11-6 22:24

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# -
+# 系统面板
+
 可以查看cpu、gpu、内存、磁盘的占用情况。
+
+## 使用
+
+#### 1.首先
+
+安装node.js
+
+ [下载 | Node.js 中文网 (nodejs.cn)](https://nodejs.cn/download/) 
+
+
+
+#### 2.安装依赖
+
+###### 首先：
+
+在项目的目录下 使用cmd命令行工具：
+
+```
+npm i
+```
+
+如果安装太慢，就运行下面的：
+
+```
+npm config set registry https://registry.npmmirror.com 
+```
+
+###### 运行:（按需要选择！）
+
+- 使用的是EventSource(SSE)
+
+```
+npm run server.js
+```
+
+​		浏览器打开：http://127.0.0.1:8081/index.html
+
+- 使用的是WebSocket
+
+```
+npm run server2.js
+```
+
+​		浏览器打开：http://127.0.0.1:8082/index2.html
+
+
+
+## 说明
+
+这只是一个简单dom。
+
+node.js后端，前端借助了EChart图表工具库。
+
+#### 效果：
+
+![](./assets/xiaoguo.png)
+
+
+
+- 左边四个可以点击选中，选中的可以在右边显示。
+
+    
+
+- 右边这个图：**鼠标放入**，按住  *ctrl+滚轮*  可以放大缩小X轴；**鼠标放入**，按住  *alt+滚轮*   可以放大缩小Y轴。
+
+- 右上角的还原按钮可以还原上述操作。
---
+++ server.js
@@ -0,0 +1,250 @@
+const os = require('os'); //获取系统内存
+const child_process = require('child_process'); //提供系统命令行服务
+const http = require('http'); //http请求服务
+const fs = require('fs'); //提供服务器读取html、css、js文件的能力
+
+
+
+// 获取内存
+// (单位,保留几位小数)
+//返回：[总内存，剩余内存]
+function getMemory(unit, n) {
+
+	let capitalization_unit = unit.toUpperCase();
+	let free = os.totalmem();
+	let total = os.freemem();
+	let free_1 = 0;
+	let total_1 = 0;
+
+	//判断单位
+	switch (capitalization_unit) {
+		case 'MB':
+			free_1 = free / Math.pow(1024, 2);
+			total_1 = total / Math.pow(1024, 2);
+			break;
+		case 'GB':
+			free_1 = free / Math.pow(1024, 3);
+			total_1 = total / Math.pow(1024, 3);
+			break;
+		case 'TB':
+			free_1 = free / Math.pow(1024, 4);
+			total_1 = total / Math.pow(1024, 4);
+			break;
+	}
+
+	//四舍五入
+	let free_2 = Number(free_1.toFixed(n));
+	let total_2 = Number(total_1.toFixed(n));
+
+	return [free_2, total_2];
+}
+
+
+
+
+
+// 获取CPU的占用率
+async function getCpu() {
+	let promise = await new Promise(resolve => {
+
+		// (回调函数,是否是剩余的)
+		let cpu = 0;
+
+		function getCPUUsage(callback, free) {
+			var stats1 = getCPUInfo();
+			var startIdle = stats1.idle;
+			var startTotal = stats1.total;
+
+			setTimeout(function() {
+				var stats2 = getCPUInfo();
+				var endIdle = stats2.idle;
+				var endTotal = stats2.total;
+
+
+				var idle = endIdle - startIdle;
+				var total = endTotal - startTotal;
+				var perc = idle / total;
+				if (free === true)
+					callback(perc);
+				else
+					callback((1 - perc));
+			}, 1000);
+		}
+
+
+		function getCPUInfo(callback) {
+			var cpus = os.cpus();
+			var user = 0;
+			var nice = 0;
+			var sys = 0;
+			var idle = 0;
+			var irq = 0;
+			var total = 0;
+			for (var cpu in cpus) {
+				user += cpus[cpu].times.user;
+				nice += cpus[cpu].times.nice;
+				sys += cpus[cpu].times.sys;
+				irq += cpus[cpu].times.irq;
+				idle += cpus[cpu].times.idle;
+			}
+			var total = user + nice + sys + idle + irq;
+			return {
+				'idle': idle,
+				'total': total
+			};
+		}
+
+		function ss(s) {
+			let h = s * 100;
+			let v = Number(h.toFixed(1));
+			cpu = v;
+
+			resolve(cpu);
+		}
+		getCPUUsage(ss, false);
+	})
+	return promise;
+}
+
+
+
+
+
+
+
+//获取磁盘空间
+// (单位,保留几位小数)
+async function getDisk(unit, n) {
+	let promise = await new Promise(resolve => {
+		let capitalization_unit = unit.toUpperCase();
+		let free = 0;
+		let total = 0;
+		child_process.exec('wmic LogicalDisk where DriveType=3 get FreeSpace,Size  /format:list', {
+			cwd: ''
+		}, (error, stdout, stderr) => {
+			if (error) return;
+			var aLines = stdout.split('\r\r\n');
+			var sFreeSpace = 0,
+				sSize = 0;
+			for (var i = 0; i < aLines.length; i++) {
+				if (aLines[i] != '') {
+					var aTokens = aLines[i].split('=');
+					switch (aTokens[0]) {
+						case 'FreeSpace':
+							sFreeSpace += Number(aTokens[1]);
+							break;
+						case 'Size':
+							sSize += Number(aTokens[1]);
+							break;
+					}
+				}
+			}
+			//判断单位
+			switch (capitalization_unit) {
+				case 'MB':
+					free = sFreeSpace / Math.pow(1024, 2);
+					total = sSize / Math.pow(1024, 2);
+					break;
+				case 'GB':
+					free = sFreeSpace / Math.pow(1024, 3);
+					total = sSize / Math.pow(1024, 3);
+					break;
+				case 'TB':
+					free = sFreeSpace / Math.pow(1024, 4);
+					total = sSize / Math.pow(1024, 4);
+					break;
+			}
+			//四舍五入
+			let free_2 = Number(free.toFixed(n));
+			let total_2 = Number(total.toFixed(n));
+			resolve([free_2, total_2]);
+		})
+	})
+	return promise;
+}
+
+
+//获取GPU占用率
+async function getGpu() {
+	return new Promise(resolve => {
+		child_process.exec('nvidia-smi -q -d UTILIZATION', {
+			cwd: ''
+		}, (error, stdout, stderr) => {
+			if (error) return;
+			let a = stdout.split('\r\n').find(s => s.indexOf('Gpu') >= 0 && s.indexOf('%') >= 0)
+			let ss = a.split(":");
+			// 去除百分号
+			var reg1 = new RegExp("%", "g");
+			var sss = ss[1].replace(reg1, "");
+			resolve(Number(sss));
+		})
+	})
+}
+
+
+
+// 创建HTTP服务器
+const server = http.createServer((req, res) => {
+	//提取url中的路径
+	let {
+		pathname
+	} = new URL(req.url, 'http://127.0.0.1');
+	//拼接出完整的路径
+	let filePath = __dirname + "/pages" + pathname;
+
+
+	if (pathname == "/data") {
+		// 设置响应头的Content-Type为text/event-stream，表示服务器将发送SSE事件
+		res.setHeader('Content-Type', 'text/event-stream');
+		res.setHeader('Cache-Control', 'no-cache');
+		res.setHeader('Connection', 'keep-alive');
+
+
+		// 每隔二秒发送一条消息
+		let interval = setInterval(() => {
+
+			let Memory = getMemory("GB", 1);
+			async function main() {
+				const [cpu, disk, gpu] = await Promise.all([getCpu(), getDisk('GB', 2), getGpu()]);
+				let data = {
+					Memory,
+					cpu,
+					disk,
+					gpu
+				}
+				const message = JSON.stringify(data);
+				res.write(`data: ${message}\n\n`);
+			}
+			main();
+		}, 1000);
+
+		// 当前请求关闭时触发：
+		req.on('close', () => {
+			res.end();
+			clearInterval(interval);
+		});
+		
+
+
+	} else {
+		filePath = __dirname + "/pages/index" + pathname;
+		fs.readFile(filePath, (err, data) => {
+			if (err) {
+				res.setHeader('content-type', 'text/html;charset=utf-8');
+				res.statusCode = 500;
+				res.end('文件读取失败~~');
+				return;
+			}
+			res.end(data);
+		});
+	}
+});
+
+
+
+
+// 监听在8081端口上
+server.listen(8081, () => {
+	console.log("入口地址：");
+	console.log("127.0.0.1:8081/index.html");
+});
